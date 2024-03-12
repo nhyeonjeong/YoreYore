b90331732c8159951831e61ae10e7e9098b3c9d6
nhyeonjeong <hjnam5962@gmail.com>
2024-03-12 13:35:06 +0900

    [FEAT] 검색화면 Cell구성 #1

--- YoreYore/Search/FoodCollectionViewCell.swift
+++ YoreYore/Search/FoodCollectionViewCell.swift
@@ -9,20 +9,38 @@ import UIKit
 import SnapKit
 
 final class FoodCollectionViewCell: BaseCollectionViewCell {
-    private let testLabel = UILabel()
+    private let foodImage: UIImageView = {
+        let view = UIImageView(frame: .zero)
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+    
+    private let foodName: UILabel = {
+        let view = UILabel()
+        view.font = Constants.Font.smallFont
+        return view
+    }()
+    
     
     override func configureHierarchy() {
-        contentView.addSubview(testLabel)
+        contentView.addSubview(foodImage)
+        contentView.addSubview(foodName)
         
     }
     
     override func configureConstraints() {
-        testLabel.snp.makeConstraints { make in
-            make.center.equalTo(contentView)
+        foodImage.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(contentView)
+            make.height.equalTo(100)
+        }
+        foodName.snp.makeConstraints { make in
+            make.top.equalTo(foodImage.snp.bottom)
+            make.horizontalEdges.bottom.equalTo(contentView)
         }
     }
+    
     func configureCell(recipe: Recipe) {
-        testLabel.text = recipe.foodName
+        foodName.text = recipe.foodName
         
     }
 }
