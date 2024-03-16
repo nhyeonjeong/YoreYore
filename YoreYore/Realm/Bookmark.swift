60f7f595dbac0f51f67eb930c0806bcd763a162d
nhyeonjeong <hjnam5962@gmail.com>
2024-03-16 23:24:58 +0900

    [FEAT] Realm 모델 구성 #8

---
+++ YoreYore/Realm/Bookmark.swift
@@ -0,0 +1,35 @@
+//
+//  RealmRecipe.swift
+//  YoreYore
+//
+//  Created by 남현정 on 2024/03/16.
+//
+
+import Foundation
+import RealmSwift
+
+class TypeFolder: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var foodType: String
+    @Persisted var bookmarkList: List<BookmarkTable> // 저장한 레시피 리스트
+
+    convenience init(foodType: String) {
+        self.init()
+        self.foodType = foodType
+    }
+}
+
+class BookmarkTable: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var foodType: String
+    @Persisted var foodName: String
+    @Persisted var kcal: String
+    @Persisted var ingredients: String
+    @Persisted var manual: List<MenualTable>
+}
+
+class MenualTable: Object {
+    @Persisted(primaryKey: true) var number: Int
+    @Persisted var manualImageString: String
+    @Persisted var manualContent: String
+}
---
+++ YoreYore/Realm/RealmRepository.swift
@@ -0,0 +1,14 @@
+//
+//  RealmRepository.swift
+//  YoreYore
+//
+//  Created by 남현정 on 2024/03/16.
+//
+
+import Foundation
+
+class RealmRepository {
+    static let shared = RealmRepository()
+    
+    func createItem(_ data: Recipe)
+}
